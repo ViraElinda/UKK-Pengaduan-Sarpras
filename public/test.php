3f96c4bc0c27aba3bed8004614da312f746683bb
Vira Elinda <viraelinda40@gmail.com>
2025-11-16 15:09:08 +0700

    test webhook

---
+++ public/test.php
@@ -0,0 +1,9 @@
+<?php
+
+// Menampilkan semua informasi PHP dan konfigurasi server.
+// Berguna untuk debugging.
+// PENTING: Hapus file ini dari server produksi setelah selesai digunakan
+// karena bisa membocorkan informasi sensitif.
+phpinfo();
+
+?>
